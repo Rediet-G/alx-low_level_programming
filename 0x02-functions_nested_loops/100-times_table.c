7edf9802f4ce12bfaef8c2ea9691f13de0499b60
Rediet-G <pommygirmay@icloud.com>
2022-04-13 16:51:31 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,11 +5,11 @@
  *                     starting eith 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(void)
+void print_times_table(int n)
 {
 int i, j, k;
 
-if (n >= 0 && <= 15)
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
